5aca894864f78efe7d5126d6ca0072b91ba3db86
Valdemar <valdemarfuglsang@gmail.com>
2025-03-12 15:47:23 +0100

    Did something

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scanpy as sc
 import anndata as ad
+from scanpy_clustering.algorithms.dbScan import db
 from sklearn.cluster import DBSCAN
 
 print('Running')
@@ -8,7 +9,7 @@ print('Running')
 adata = sc.read_h5ad('data\symsim_observed_counts_5000genes_5000cells_complex.h5ad')  # Example dataset from Scanpy
 
 # Apply DBScan
-adata = dbscan_ann_data(adata, eps=0.5, min_samples=10)
+adata = db(adata, eps=0.5, min_samples=10)
 
 # Check results
 print(adata.obs['dbscan_labels'].value_counts())
