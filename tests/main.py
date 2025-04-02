9b781c5d1d26ec23b5551ef3b4e76e8719e8015c
Svendsen02 <sebastianthyregod@gmail.com>
2025-04-02 10:24:00 +0200

    Nothing has changed

--- tests/main.py
+++ tests/main.py
@@ -5,13 +5,13 @@ from scanpy_clustering import clustering as cl
 
 print('Running')
 # Load example data
-adata = sc.read_h5ad('data\\symsim_observed_counts_5000genes_5000cells_complex.h5ad')  # Example dataset from Scanpy
+adata = sc.read_h5ad('data\\symsim_observed_counts_5000genes_5000cells_complex.h5ad')[:750, :30]  # Example dataset from Scanpy
 
 # Apply DBScan
 #cl.enable_scanpy_integration()
-print(cl.list_algorithms())
+#print(cl.list_algorithms())
 #sc.tl.DBScan(adata, key_added='dbscan_labels', eps=0.5, min_samples=10, metric='euclidean')
 #sc.tl.DBScan.cluster(adata, eps=0.5, min_samples=10, metric='euclidean')
-#cl.cluster(adata, algorithm='DBScanOLD', key_added='dbscan_labels', eps=0.5, min_samples=10, metric='euclidean')
+cl.cluster(adata, algorithm='DBSCANOPT', key_added='dbscan_labels', eps=2500, min_samples=30, metric='euclidean')
 # Check results
 #print(adata.obs['dbscan_labels'].value_counts())
