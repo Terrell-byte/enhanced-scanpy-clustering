e43be4087252e2e37d2a5a647a23bcdb2072a8c6
Svendsen02 <sebastianthyregod@gmail.com>
2025-04-02 11:49:35 +0200

    Make good

--- tests/main.py
+++ tests/main.py
@@ -8,13 +8,12 @@ from scipy.spatial import KDTree
 
 print('Running')
 # Load example data
-adata = sc.read_h5ad('data\\symsim_observed_counts_5000genes_5000cells_complex.h5ad')[:750, :30]  # Example dataset from Scanpy
-
+adata = sc.read_h5ad('data\\symsim_true_counts_5000genes_1000cells_complex.h5ad') # Example dataset from Scanpy
+'''
 # Load example dataset (replace with your AnnData object)
 #adata = sc.datasets.pbmc3k()
 X = adata.X.toarray() if hasattr(adata.X, "toarray") else adata.X  # Convert sparse if needed
 
-'''
 # Set k (min_samples for DBSCAN, usually 4 or 5)
 k = 5
 
@@ -37,6 +36,6 @@ plt.title(f"k-Distance Graph for k={k}")
 plt.grid()
 plt.show() '''
 
-cl.cluster(adata, algorithm='DBSCANOPT', key_added='dbscan_labels', eps=25, min_samples=4, metric='euclidean')
+cl.cluster(adata, algorithm='DBSCANOPT', key_added='dbscan_labels', eps=22500, min_samples=7, metric='euclidean')
 # Check results
 print(adata.obs)
