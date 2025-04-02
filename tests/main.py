11d3b67b125d4c18fe2bf657106a09366cfff3a3
Valdemar <valdemarfuglsang@gmail.com>
2025-04-02 11:23:47 +0200

    Cleaned main

--- tests/main.py
+++ tests/main.py
@@ -1,17 +1,28 @@
+import importlib
 import numpy as np
 import scanpy as sc
 import anndata as ad
 import scanpy_clustering.clustering as cl
+import time
+from sklearn.metrics.cluster import adjusted_rand_score
 
 print('Running')
 # Load example data
 adata = sc.read_h5ad('data\\symsim_observed_counts_5000genes_5000cells_complex.h5ad')[:750, :30]  # Example dataset from Scanpy
 
-# Apply DBScan
-#cl.enable_scanpy_integration()
-#print(cl.list_algorithms())
-#sc.tl.DBScan(adata, key_added='dbscan_labels', eps=0.5, min_samples=10, metric='euclidean')
-#sc.tl.DBScan.cluster(adata, eps=0.5, min_samples=10, metric='euclidean')
-cl.cluster(adata, algorithm='DBSCANOPT', key_added='dbscan_labels', eps=2500, min_samples=30, metric='euclidean')
+# Start timer
+tic = time.perf_counter()
+
+# Run DBScan clustering with custom implementation
+cl.cluster(adata, algorithm='DBScan_Base', key_added='dbscan_labels', eps=25, min_samples=4, metric='euclidean')
+
+# Get time taken for custom implementation
+toc = time.perf_counter()
+print(f"Ran in: {toc - tic:0.4f} seconds")
+
 # Check results
-#print(adata.obs['dbscan_labels'].value_counts())
+print(adata.obs)
+
+# Check rand_score
+from sklearn.metrics.cluster import adjusted_rand_score
+adjusted_rand_score(adata.obs['group'], adata.obs['dbscan_labels'])
