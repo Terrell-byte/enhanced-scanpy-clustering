f97ebfa7a981b523cde42edfa2f1b0db7576c9c7
Valdemar <valdemarfuglsang@gmail.com>
2025-04-02 11:47:44 +0200

    Fixed input vals for DBScan

--- tests/main.py
+++ tests/main.py
@@ -11,13 +11,13 @@ from scipy.spatial import KDTree
 
 print('Running')
 # Load example data
-adata = sc.read_h5ad('data\\symsim_observed_counts_5000genes_5000cells_complex.h5ad')[:750, :30]  # Example dataset from Scanpy
+adata = sc.read_h5ad('data\\symsim_observed_counts_5000genes_5000cells_complex.h5ad')[:7, :3]  # Example dataset from Scanpy
 
-# Load example dataset (replace with your AnnData object)
-#adata = sc.datasets.pbmc3k()
-X = adata.X.toarray() if hasattr(adata.X, "toarray") else adata.X  # Convert sparse if needed
 
 '''
+# Load example dataset (replace with your AnnData object)
+adata = sc.datasets.pbmc3k()
+X = adata.X.toarray() if hasattr(adata.X, "toarray") else adata.X  # Convert sparse if needed
 # Set k (min_samples for DBSCAN, usually 4 or 5)
 k = 5
 
@@ -44,7 +44,7 @@ plt.show() '''
 tic = time.perf_counter()
 
 # Run DBScan clustering with custom implementation
-cl.cluster(adata, algorithm='DBScan_Base', key_added='dbscan_labels', eps=25, min_samples=4, metric='euclidean')
+cl.cluster(adata, algorithm='DBSCANOPT', key_added='dbscan_labels', eps=25, min_samples=4, metric='euclidean')
 
 # Get time taken for custom implementation
 toc = time.perf_counter()
@@ -55,4 +55,4 @@ print(adata.obs)
 
 # Check rand_score
 from sklearn.metrics.cluster import adjusted_rand_score
-adjusted_rand_score(adata.obs['group'], adata.obs['dbscan_labels'])
+print(f"Rand_score: {adjusted_rand_score(adata.obs['group'], adata.obs['dbscan_labels'])} ")
