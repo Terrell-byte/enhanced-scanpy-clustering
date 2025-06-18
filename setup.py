79689535f887ea89c410da36d5eb7a3ce4e9abfc
Valdemar <valdemarfuglsang@gmail.com>
2025-06-18 10:54:57 +0200

    Added scikit learn requirement to setup

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name="rna_clustering_algorithms",
     version="0.1.4",
     description="Modular clustering extension for scanpy",
-    author=["Daniel Sutton", "Laurits Madsen", "Sebastian Svendsen", "Valdemar fuglsang"],
+    author=["Daniel Sutton", "Laurits Madsen", "Sebastian Svendsen", "Valdemar Fuglsang"],
     packages=find_packages(),
     install_requires=[
         "scanpy>=1.9.0",
         "numpy>=1.20.0",
         "anndata>=0.8.0",
-        "scipy>=1.6.0",
+        "scipy>=1.8.0",
+        "scikit-learn>=1.7.0",
     ],
     python_requires=">=3.8",
 ) 
